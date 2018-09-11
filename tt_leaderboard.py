abe104164cba82cf7e907248b633c17bfc48911d
Sandeep Sharda <sandeepsharda@Marvins-MBP-3.localdomain>
2018-09-11 12:00:37 +0100

    added prettytable module to show leaderboard

--- tt_leaderboard.py
+++ tt_leaderboard.py
@@ -1,5 +1,6 @@
 from leaderboard import Leaderboard
 from player import Player
+from prettytable import PrettyTable
 import sys
 
 leaderboard = Leaderboard()
@@ -12,10 +13,18 @@ def enter_score(winner_name, loser_name):
     leaderboard.update_leaderboard(winner_name, loser_name)
 
 def show_leaderboard():
-    for player in leaderboard.get_players():
-        print(player.get_name())
+    leader_table = PrettyTable()
+    leader_table.field_names = ["Rank", "Player Name"]
 
-commands = ["-add", "-score", "-show"]
+    for index, player in enumerate(get_leaderboard_players()):
+        leader_table.add_row([index + 1, player.get_name().capitalize()])
+        
+    print(leader_table)
+
+def get_leaderboard_players():
+    return leaderboard.get_players()
+
+commands = ["-add", "-score", "-show", "-help"]
 
 def main():
 
@@ -28,23 +37,39 @@ def main():
     if len(args) > 0 and args[0] in commands: 
         command_processor(args)
     else:
-        print("These are the available options:" + str(commands))
+        help(None)
         sys.exit(1)
 
 def command_processor(args):
     if args[0] == "-add":
-        add_player(args[1])
-        print("> Added: " + args[1])
-
-    if args[0] == "-score":
-        enter_score(args[1], args[2])
-        print("> Score: %s beat %s" % (args[1], args[2]))
-
-    if args[0] == "-show":
+        if len(args) == 2:
+            add_player(args[1])
+            print("> Added: " + args[1])
+        else:
+            help(args[0])
+    elif args[0] == "-score":
+        if len(args) == 3:
+            enter_score(args[1], args[2])
+            print("> Score: %s beat %s" % (args[1], args[2]))
+        else:
+            help(args[0])
+    elif args[0] == "-show":
         show_leaderboard()
+    elif args[0] == "-help":
+        help(None)
 
     write_leaderboard()
 
+def help(command):
+    if command == "-score":
+        print("score: incorrect usage")
+        print("usage: -score [winner name] [loser name]")
+    elif command is None:      
+        print("These are the available options:" + str(commands))
+    elif command == "-add":
+        print("add: incorrect usage")
+        print("usage: -add [player name]")
+        
 def sanitise_input(input_arr):
     san = []
     for item in input_arr:
@@ -58,7 +83,6 @@ def write_leaderboard():
     with open("leaderboard.csv", "w") as f:
         for player in leaderboard.get_players():
             f.write(str(player.name) + "\n")
-    f.close()
 
 def read_leaderboard():
     names = []
